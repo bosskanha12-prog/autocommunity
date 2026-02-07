09688daf2f3cc271b8615adfc3ee7f9999427194
bosskanha12-prog <bosskanha12@gmail.com>
2026-02-07 10:41:36 +0530

    Update poll_builder.py

--- src/poll_builder.py
+++ src/poll_builder.py
@@ -1,1 +1,5 @@
-
+def build_image_poll(question, options):
+    post = f"🖼️ IMAGE POLL\n\n🗳️ {question}\n\n"
+    post += "\n".join(options)
+    post += "\n\n👇 Comment A / B / C / D"
+    return post
