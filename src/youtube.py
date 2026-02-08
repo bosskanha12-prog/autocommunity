6fea9123f6f130c3c943cfa995f6ad8ed0e78aec
bosskanha12-prog <bosskanha12@gmail.com>
2026-02-08 12:41:21 +0530

    Update youtube.py

--- src/youtube.py
+++ src/youtube.py
@@ -2,16 +2,16 @@ import os
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
 
 SCOPES = [
     "https://www.googleapis.com/auth/youtube.upload",
     "https://www.googleapis.com/auth/youtube.readonly",
 ]
 
+
 def get_youtube_client():
     creds = Credentials(
-        token=None,  # always refresh in GitHub Actions
+        token=None,
         refresh_token=os.environ["YOUTUBE_REFRESH_TOKEN"],
         token_uri="https://oauth2.googleapis.com/token",
         client_id=os.environ["YOUTUBE_CLIENT_ID"],
@@ -20,18 +20,11 @@ def get_youtube_client():
     )
 
     creds.refresh(Request())
-
     return build("youtube", "v3", credentials=creds)
 
 
-def upload_community_post(text, image_path):
+def upload_community_post(text):
     youtube = get_youtube_client()
-
-    media = MediaFileUpload(
-        image_path,
-        mimetype="image/png",
-        resumable=False
-    )
 
     request = youtube.activities().insert(
         part="snippet,contentDetails",
@@ -42,9 +35,7 @@ def upload_community_post(text, image_path):
             "contentDetails": {
                 "bulletin": {}
             }
-        },
-        media_body=media
+        }
     )
 
-    response = request.execute()
-    return response
+    return request.execute()
